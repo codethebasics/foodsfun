70cd5d69900f61476514eebb38183d6081897a6f
codethebasics <codethebasics@gmail.com>
2022-08-04 22:01:39 -0300

    setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: "avoid",
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  semi: false,
+  tabWidth: 3,
+  trailingComma: "none",
+};
+
+module.exports = options;
